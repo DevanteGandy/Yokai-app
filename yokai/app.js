bc798187b713a722d06ae4abab0c5e6dcc0e35cd
Devante Gandy <vante@Devantes-MacBook-Pro.local>
2021-04-10 16:59:02 -0400

    added image row, working on modal gallery

--- yokai/app.js
+++ yokai/app.js
@@ -1,22 +1,30 @@
 $(() => {
+  const array = [];
   $.ajax({
-      url:'https://kitsu.io/api/edge/anime?filter[categories]=horror'
+      url:'https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0&filter[categories]=horror'
   }).then(
-      (data)=>{
-        const $imgUrl = $('<img>').attr('src', data.data[0].attributes.posterImage.tiny).addClass('innerImg');
-        $('.displayCovers').append($imgUrl)
-        $('#animeTitle').html(data.data[0].attributes.canonicalTitle);
-      })
+    (data)=>{
+      for ( i = 0; i < 12; i++) {
+        const $imgUrl = $('<img>').attr('src', data.data[i].attributes.posterImage.tiny)
+        // const $coverTitle = $('<div>').text(data.data[i].attributes.canonicalTitle);
+        array.push(data.data[i].attributes.posterImage.small)
+        const $modal = $('#Modal');
+        const $closeBtn = $('#close');
+        const $openBtn = $($imgUrl)
+
+        const openModal = () => {
+          $modal.css('display', 'block');
+        }
+        const closeModal = () => {
+          $modal.css('display', 'none');
+        }
+
+        $openBtn.on('click', openModal);
+        $closeBtn.on('click', closeModal);
 
 
-
-
-
-
-
-
-
-
-
-
+        $('.displayCovers').append($imgUrl);
+      }
+    }
+  )
 })
