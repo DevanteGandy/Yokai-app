de9be3e5bdd0578ca00777a888015a7883ed0bc1
Devante Gandy <vante@Devantes-MacBook-Pro.local>
2021-04-10 22:37:58 -0400

    added horror imgs, supernatural imgs,

--- yokai/app.js
+++ yokai/app.js
@@ -1,16 +1,29 @@
 $(() => {
-  const array = [];
+  const imgArray = [];
+  const titleArray = [];
+  const synopArray = [];
+  const dateArray = [];
+  // let imgLink = null;
   $.ajax({
       url:'https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0&filter[categories]=horror'
   }).then(
     (data)=>{
       for ( i = 0; i < 12; i++) {
-        const $imgUrl = $('<img>').attr('src', data.data[i].attributes.posterImage.tiny)
-        // const $coverTitle = $('<div>').text(data.data[i].attributes.canonicalTitle);
-        array.push(data.data[i].attributes.posterImage.small)
+        const $imgUrl = $('<img>').attr('src', data.data[i].attributes.posterImage.tiny).attr('value',i).addClass('displayCover')
+        // const $boxTitle = $('<div>').text(data.data[i].attributes.canonicalTitle).attr('value',i);
+        // const $boxSynop = $('<div>').text(data.data[i].attributes.synopsis).attr('value',i);
+        // const $boxDate = $('<div>').text(data.data[i].attributes.startDate).attr('value',i);
+
+
+        imgArray.push(data.data[i].attributes.posterImage.tiny)
+        titleArray.push(data.data[i].attributes.titles.en_jp)
+        synopArray.push(data.data[i].attributes.synopsis)
+        dateArray.push(data.data[i].attributes.startDate)
+
         const $modal = $('#Modal');
         const $closeBtn = $('#close');
-        const $openBtn = $($imgUrl)
+        const $openBtn = $($imgUrl);
+
 
         const openModal = () => {
           $modal.css('display', 'block');
@@ -24,7 +37,69 @@ $(() => {
 
 
         $('.displayCovers').append($imgUrl);
-      }
+        }
+        $('.displayCover').on('click', (event) => {
+          $('#innerImg').empty()
+          $('#textLayout').empty()
+          $('#innerImg').append($('<img>').attr('src', imgArray[$(event.target).attr('value')]))
+          $('#textLayout').append($('<h1>').text(titleArray[$(event.target).attr('value')]))
+          $('#textLayout').append($('<h2>').text(synopArray[$(event.target).attr('value')]))
+          $('#textLayout').append($('<p>').text(dateArray[$(event.target).attr('value')]))
+
+
+
+      })
+    }
+  )
+})
+$(() => {
+  const imgArray = [];
+  const titleArray = [];
+  const synopArray = [];
+  const dateArray = [];
+  // let imgLink = null;
+  $.ajax({
+      url:'https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=10&filter[categories]=supernatural'
+  }).then(
+    (data)=>{
+      for ( i = 0; i < 12; i++) {
+        const $imgUrl = $('<img>').attr('src', data.data[i].attributes.posterImage.tiny).attr('value',i).addClass('displayCover')
+        // const $boxTitle = $('<div>').text(data.data[i].attributes.canonicalTitle).attr('value',i);
+        // const $boxSynop = $('<div>').text(data.data[i].attributes.synopsis).attr('value',i);
+        // const $boxDate = $('<div>').text(data.data[i].attributes.startDate).attr('value',i);
+
+
+        imgArray.push(data.data[i].attributes.posterImage.tiny)
+        titleArray.push(data.data[i].attributes.titles.en_jp)
+        synopArray.push(data.data[i].attributes.synopsis)
+        dateArray.push(data.data[i].attributes.startDate)
+
+        const $modal = $('#Modal');
+        const $closeBtn = $('#close');
+        const $openBtn = $($imgUrl);
+
+
+        const openModal = () => {
+          $modal.css('display', 'block');
+        }
+        const closeModal = () => {
+          $modal.css('display', 'none');
+        }
+
+        $openBtn.on('click', openModal);
+        $closeBtn.on('click', closeModal);
+
+
+        $('.superNRow').append($imgUrl);
+        }
+        $('.superNRow').on('click', (event) => {
+          $('#innerImg').empty()
+          $('#textLayout').empty()
+          $('#innerImg').append($('<img>').attr('src', imgArray[$(event.target).attr('value')]))
+          $('#textLayout').append($('<h1>').text(titleArray[$(event.target).attr('value')]))
+          $('#textLayout').append($('<h2>').text(synopArray[$(event.target).attr('value')]))
+          $('#textLayout').append($('<p>').text(dateArray[$(event.target).attr('value')]))
+      })
     }
   )
 })
